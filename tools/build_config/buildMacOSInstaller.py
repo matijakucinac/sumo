98d340655d9f7a7c5a6fa0af16813b644d053d08
Michael Behrisch <michael.behrisch@dlr.de>
2026-02-03 20:30:45 +0100

    fixing macos installer path #3

--- tools/build_config/buildMacOSInstaller.py
+++ tools/build_config/buildMacOSInstaller.py
@@ -50,7 +50,7 @@ def parse_args(def_dmg_name, def_pkg_name):
     def_output_apps_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "..", "apps"))
     def_output_fw_pkg_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "..", "framework-pkg"))
     def_output_apps_pkg_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "..", "apps-pkg"))
-    def_output_pkg_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "..",
+    def_output_pkg_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..",
                                                        "installer", def_pkg_name))
     def_output_dmg_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", def_dmg_name))
 
